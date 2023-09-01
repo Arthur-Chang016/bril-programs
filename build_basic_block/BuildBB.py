36f6c55543d245f32cfd93cb5d6a9b9b1f42fe28
Arthur-Chang016 <b05505016@g.ntu.edu.tw>
2023-09-01 08:31:47 +0800

    Add file and read file

--- build_basic_block/BuildBB.py
+++ build_basic_block/BuildBB.py
@@ -1,6 +1,16 @@
 
+
+import json
+import sys
+
+
+def CountNumberOfBasicBlocks(func):
+    return
+
 def main():
-    print("Hello World!")
+    # read file
+    brilJson = json.loads(sys.stdin.read())
+    print(brilJson)
 
 if __name__ == "__main__":
     main()
