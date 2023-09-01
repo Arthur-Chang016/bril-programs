26ddf6e8c5702b18f24f27f00bae8dd8dbc1b367
Arthur-Chang016 <b05505016@g.ntu.edu.tw>
2023-09-01 08:57:34 +0800

    Basic Implementation

--- build_basic_block/BuildBB.py
+++ build_basic_block/BuildBB.py
@@ -3,14 +3,35 @@
 import json
 import sys
 
+def isControlInst(inst):
+    return "op" in inst and (inst["op"] == "jmp" or inst["op"] == "br")
 
+def isLabel(inst):
+    return "label" in inst
+
+# BB will start with a label or end with a control instruction
 def CountNumberOfBasicBlocks(func):
-    return
+    instList = func["instrs"]
+    n = len(instList)
+    cnt = 1
+    
+    for i in range(0, n - 1):
+        cur = instList[i]; next = instList[i + 1]
+        isBound = isControlInst(cur) or isLabel(next)
+        if isBound: cnt += 1
+    
+    return cnt
+    
 
 def main():
     # read file
     brilJson = json.loads(sys.stdin.read())
-    print(brilJson)
+    
+    functions = brilJson["functions"]
+    for func in functions:
+        BBs = CountNumberOfBasicBlocks(func)
+        funcName = func["name"]
+        print(f"{funcName}:{BBs}")
 
 if __name__ == "__main__":
     main()
