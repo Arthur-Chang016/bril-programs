4f121bdb57f48d5d04f19f14bb77f7947c5e5588
Arthur-Chang016 <b05505016@g.ntu.edu.tw>
2023-09-08 02:37:50 +0800

    Add LVN framework

--- include/BasicBlock.hpp
+++ include/BasicBlock.hpp
@@ -1,4 +1,12 @@
 
 class BasicBlock {
+public:
+    vector<json> instrs;
+    
+    
+    // methods
+    void push_back(json inst) {
+        instrs.push_back(inst);
+    }
     
 };
--- opt/LVN.cpp
+++ opt/LVN.cpp
@@ -1,6 +1,8 @@
 // STL
 #include <fstream>
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
 // json lib: https://github.com/nlohmann/json
@@ -14,9 +16,30 @@ json ReadStdin() {
     return json::parse(cin);
 }
 
-vector<BasicBlock> BuildBasicBlock() {
+vector<BasicBlock> BuildBasicBlock(json instrs) {
+    // TODO
     
-    return {};
+    BasicBlock bb;
+    for (json &inst: instrs) {
+        bb.push_back(inst);
+    }
+    
+    return {bb};
+}
+
+json convertToInst(vector<BasicBlock> &BBs) {
+    json retInstrs = json::array();
+    for(BasicBlock &bb: BBs) {
+        for(json &inst: bb.instrs) {
+            retInstrs.push_back(inst);
+        }
+    }
+    return retInstrs;
+}
+
+BasicBlock LVN(BasicBlock &bb) {
+    // TODO
+    return bb;
 }
 
 int main(int argc, char **argv) {
@@ -24,10 +47,16 @@ int main(int argc, char **argv) {
     
     // cout << program << endl;
     
-    for(auto func: program["functions"]) {
-        cout << func << endl;
+    for(auto &func: program["functions"]) {
+        vector<BasicBlock> BBs = BuildBasicBlock(func["instrs"]);
+        
+        for(BasicBlock &bb: BBs) {
+            bb = LVN(bb);
+        }
+        func["instrs"] = convertToInst(BBs);
     }
     
+    cout << program << endl;
     
     
     return 0;
